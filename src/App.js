94ec5a965a7b0491c103278c19de15a3d37067a8
thivankadilshangithub <thivankadilshan726@gmail.com>
2022-07-07 18:27:41 +0530

    Fuctional update commit

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ state = {
 }
 
 async componentDidMount(){
-  console.log(process.env.REACT_APP_GITHUB_CLIENT_SECRET);
+  // console.log(process.env.REACT_APP_GITHUB_CLIENT_SECRET);
 
   this.setState({ loading: true});
   const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
